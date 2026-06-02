ce6a434711276129b065301c492304239c7e6618
naitik3108-hash <naitikyadav3108@gmail.com>
2026-06-02 18:10:58 +0530

    Update #1 Two Sum.java

--- DSA/ARRAY/#1 Two Sum.java
+++ DSA/ARRAY/#1 Two Sum.java
@@ -1,8 +1,5 @@
-
-import java.util.HashMap;
-
-// Using Brute Force
-/*class Solution {
+// BRUTE FORCE
+class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0 ;i<nums.length ;i++){
             for (int j=i+1 ; j<nums.length ;j++){
@@ -13,27 +10,5 @@ import java.util.HashMap;
 
         }
         return new int []{}; 
-    }
-}
-*/
-
-// Using Hash Tabble
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map =new HashMap<>();
-      
-        for (int i=0 ;i<nums.length ;i++){
-            int remain=target-nums[i] ;
-            if (map.containsKey(remain)){
-                return new int []{i,map.get(remain)}  ;
-            }
-            else {
-            map.put(nums[i],i);
-
-            }
-        }
-
-        return new int []{} ;
-
     }
 }
